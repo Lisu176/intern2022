9c57bc496d04e68e432cc605732184b8894c4dd9
maeno <oqlas.sdcn.1isu@gmail.com>
2022-10-25 11:15:04 +0900

    投稿のstyle更新

--- server/resources/views/post/create.blade.php
+++ server/resources/views/post/create.blade.php
@@ -5,17 +5,17 @@
         <form action="{{ route('post.store') }}" method="POST" >
             @csrf
             <div>
-                名前：<input name="name" placeholder="名前の入力欄"/>
+                名前<br><input name="name" placeholder="名前の入力欄"/>
+            </div>
+            <div style="margin: 10px 0 0 0;">
+                タイトル<br><input name="title" placeholder="タイトルの入力欄"/>
+            </div>
+            <div style="margin: 10px 0 0 0;">
+                コメント<br><textarea name="comment" placeholder="内容の入力"></textarea>
             </div>
             <div>
-                タイトル：<input name="title" placeholder="タイトルの入力欄"/>
-            </div>
-            <div>
-                <textarea name="comment" placeholder="内容の入力"></textarea>
-            </div>
-            <div>
-                Test<input type="checkbox" name="category_id" value="0" {{ old("category_id") == 1 ? "checked" : "" }} />
-                edit_log<input type="checkbox" name="category_id" value="1" {{ old("category_id") == 2 ? "checked" : "" }} />
+                Test<input type="checkbox" name="category_id" value="0"/>
+                edit_log<input type="checkbox" name="category_id" value="1"/>
             </div>
             <button>送信</button>
         </form>
