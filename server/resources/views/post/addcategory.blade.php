8366b51e444433097b9d354a1c7f220a59374b89
maeno <oqlas.sdcn.1isu@gmail.com>
2022-10-27 10:18:56 +0900

    “カテゴリ追加用”

---
+++ server/resources/views/post/addcategory.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.layout')
+
+@section('content')
+    <div style="margin:0 auto; text-align:center;">
+        <form action="{{ route('post.add') }}" method="POST" >
+            @csrf
+            <div>
+                カテゴリ名<br><input name="category" placeholder="カテゴリの入力欄"/>
+            </div>
+            <button>送信</button>
+        </form>
+    </div>
+@endsection
